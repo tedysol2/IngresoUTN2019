2fbf2ecb971bd5e45609dccd3ab2d92d19528205
tedysol2 <47386529+tedysol2@users.noreply.github.com>
2019-02-07 12:28:38 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,13 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+var sueldo;
+var nuevosueldo;
+var aumento;
+var resultado;
 
+sueldo = document.getElementById ("sueldo").value;
+resultado = document.getElementById ("resultado").value;
 	
+    alert ("mostrar aumento" + resulrado);
 }
