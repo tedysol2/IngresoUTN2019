45e2e060aed2e528881028c8d919d6afb3f85ea3
tedysol2 <47386529+tedysol2@users.noreply.github.com>
2019-02-15 23:02:46 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -2,6 +2,11 @@ function Mostrar()
 {
 //tomo la edad  
 
+var edad;
+ edad = parseInt(document.getElementById ("edad").value);
+ if( edad == 15){
+     alert("niña bonita")
+ }
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -2,6 +2,11 @@ function Mostrar()
 {
 //tomo la edad  
 
+var edad;
+edad= parseInt(document.getElementById ("edad").value);
+if (edad > 17){
+    alert( "la persona es mayor de edad")
+}
 
 
 }//FIN DE LA FUNCIÓN
